36b2e71fc9046b7332ed3998a3edbed9805244c1
lrakarl <stephene.bernardo@gmail.com>
2018-09-21 00:09:14 +0800

    refactored database.js

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const app = express()
 const port = 3000
 var bodyParser = require('body-parser')
 var jwt = require('jsonwebtoken');
-const data = require('./src/database')
+const DatabaseSetup = require('./src/database')
 
-data.databaseSetup().then(function(){
+DatabaseSetup.execute().then(function () {
     app.use(bodyParser.json())
     app.post('/auth', (req, res) => {
-    
+
         var token = jwt.sign(req.body, 'shhhhh');
         res.send(token)
     })
-    
-    app.get('/verifyToken', (req,res) => {
-        jwt.verify(req.headers.token, 'shhhhh', function(err, decoded) {
-            if(err){
+
+    app.get('/verifyToken', (req, res) => {
+        jwt.verify(req.headers.token, 'shhhhh', function (err, decoded) {
+            if (err) {
                 res.send("Authentication failed")
             }
             res.send(decoded)
         });
     })
-    
+
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 });
--- src/database.js
+++ src/database.js
@@ -3,39 +3,44 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./auth.db');
 var Promise = require('promise');
 
-module.exports = {
-    databaseSetup: function () {
-        return new Promise(function(fulfill)  {
-            db.serialize(function() {
-                db.each("select * from user_info", function(err,row) {
-                    if(!row){
-                        db.serialize(function(){
-                            createUserInfoTable()
-                            setUpDefaultUsers()
-                            showAllUsers(fulfill)    
-                        })
-                    } 
-                })
-              });
-        })
+class DatabaseSetup {
+    static execute() {
+        return new Promise((fulfill) => this.createTableAndGenerateUsers(fulfill))
+    }
+
+    static createTableAndGenerateUsers(callback) {
+        let that = this;
+        db.serialize(function () {
+            db.each("select * from user_info", function (err, row) {
+                if (!row) {
+                    db.serialize(function () {
+                        that.createUserInfoTable()
+                        that.setUpDefaultUsers()
+                        that.showAllUsers(callback)
+                    })
+                }
+            })
+        });
+    }
+    static createUserInfoTable() {
+        db.run("CREATE TABLE user_info (username TEXT UNIQUE, password TEXT, name TEXT)");
+    }
+    static showAllUsers(fulfill) {
+        db.each("SELECT * FROM user_info", function (err, row) {
+            console.log(row);
+        }, fulfill);
+    }
+    static setUpDefaultUsers() {
+
+        var stmt = db.prepare("INSERT INTO user_info VALUES (?, ?, ?)");
+
+        stmt.run("kbernard", "secret", "karl bernardo");
+        stmt.run("foo", "bar", "foo bar")
+        stmt.run("spammy", "secret", "spammy pizzo")
+
+        stmt.finalize();
     }
 }
 
-function createUserInfoTable(){
-    db.run("CREATE TABLE user_info (username TEXT UNIQUE, password TEXT, name TEXT)"); 
-}
-function showAllUsers(fulfill){
-    db.each("SELECT * FROM user_info", function(err, row) {
-        console.log(row);
-    }, fulfill);
-}
-function setUpDefaultUsers(){
-    
-    var stmt = db.prepare("INSERT INTO user_info VALUES (?, ?, ?)");
-        
-    stmt.run("kbernard", "secret", "karl bernardo");
-    stmt.run("foo", "bar", "foo bar")
-    stmt.run("spammy", "secret", "spammy pizzo")
 
-    stmt.finalize();
-}
+module.exports = DatabaseSetup;
